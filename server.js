c133653f3fda6dd979e78ed65353182cd0aab922
setyawan12 <bocahgendut005@gmail.com>
2022-10-17 00:35:30 +0700

    latihan

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { json } = require('express/lib/response');
+const mysql = require('mysql')
+const BodyParser = require('body-parser')
+const app = express();
+app.use(BodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    database: "siswa",
+    user: "root",
+    password: "",
+})
+
+db.connect((err) =>{
+    if(err) throw err
+    console.log('database tersambung')
+
+    app.get("/", (req, res) => {
+        const sql = "SELECT * FROM user"
+        db.query(sql, (err,result) => {
+            const users = JSON.parse(JSON.stringify(result))
+            res.render("index", { users: users, title: "Daftar Murid"})
+        })
+        
+    })
+
+    app.post("/tambah", (req, res) =>{
+        const insertSql = `INSERT INTO user (nama, kelas) VALUES ('${req.body.nama}', '${req.body.kelas}');`
+        db.query(insertSql, (err, result) => {
+            if(err) throw err
+            res.redirect("/")
+        })
+    })
+  
+})
+app.listen(8000, () => {
+    console.log("Server Ready...")
+})
